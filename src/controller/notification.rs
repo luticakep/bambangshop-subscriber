fcb3e9a6d215c31c295bc57045db940c25572478
luticakep <kaylasorayadj@gmail.com>
2025-03-26 00:40:38 +0700

    Implement the receive function in Notification controller

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -8,15 +8,23 @@ use crate::service::notification::NotificationService;
 #[get("/subscribe/<product_type>")]
 pub fn subscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     return match NotificationService::subscribe(product_type) {
-        Ok(x) => Ok(Json(x)),
-        Err(y) => Err(y)
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
     }
 }
 
 #[get("/unsubscribe/<product_type>")]
 pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     return match NotificationService::unsubscribe(product_type) {
-        Ok(x) => Ok(Json(x)),
-        Err(y) => Err(y)
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
+    }
+}
+
+#[post("/receive", data = "<notification>")]
+pub fn receive(notification: Json<Notification>) -> Result<Json<Notification>> {
+    return match NotificationService::recieve_notification(notification.into_inner()) {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
     }
 }
